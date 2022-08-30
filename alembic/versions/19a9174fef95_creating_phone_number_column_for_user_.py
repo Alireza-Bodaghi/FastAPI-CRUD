5f7e9725c565f9201c497854957033bc5400fea6
Alireza Bodaghi <bodaghi1377@gmail.com>
2022-08-30 11:45:56 +0430

    TST: configuring alembic and test it with column phone number

---
+++ alembic/versions/19a9174fef95_creating_phone_number_column_for_user_.py
@@ -0,0 +1,36 @@
+"""creating phone number column for user table
+
+Revision ID: 19a9174fef95
+Revises: 
+Create Date: 2022-08-30 11:24:42.140738
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '19a9174fef95'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+# in order to initialize alembic for your project
+# use: alembic init <your folder-name goes here> command
+
+
+# in order to create a revision, use:
+# alembic revision -m <your message goes here> command
+
+
+# in order to run upgrade, use:
+# alembic upgrade <revision number>
+
+def upgrade() -> None:
+    op.add_column('user', sa.Column('phone_number', sa.String(), nullable=True))
+
+
+# in order to run downgrade, use:
+# alembic downgrade -1
+def downgrade() -> None:
+    op.drop_column('user', 'phone_number')
